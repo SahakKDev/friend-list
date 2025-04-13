f3f2303a54ab116a127865954dd27f07c6af2dc1
Sahak Keshishyan <sahak.keshishyan.dev@gmail.com>
2025-04-13 12:07:22 +0400

    first commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,56 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ friendIsFavorite === '1' ? '(Favorite)' : '' }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>Email:</strong> {{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friendIsFavorite: this.isFavorite,
+    }
+  },
+  methods: {
+    toggleFavorite() {
+      if (this.friendIsFavorite === '1') {
+        this.friendIsFavorite = '0'
+      } else {
+        this.friendIsFavorite = '1'
+      }
+    },
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible
+    },
+  },
+  // props: ['name', 'phoneNumber', 'emailAddress', 'isFavorite'],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: String,
+      default: '0',
+      validator(value) {
+        return value === '1' || value === '0'
+      },
+    },
+  },
+}
+</script>
